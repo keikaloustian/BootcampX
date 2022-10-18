3960a6bee9e00c823591df15a5d0091dba5bfff3
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-17 17:11:26 -0700

    Refactor - ON clause on same line as preceding JOIN

--- 2_queries_joins/1_student_total_assig_duration.sql
+++ 2_queries_joins/1_student_total_assig_duration.sql
@@ -1,5 +1,4 @@
 SELECT SUM(duration) as total_duration
 FROM students
-JOIN assignment_submissions
-ON students.id = student_id
+JOIN assignment_submissions ON students.id = student_id
 WHERE name = 'Ibrahim Schimmel';
--- 2_queries_joins/2_cohort_total_assig_duration.sql
+++ 2_queries_joins/2_cohort_total_assig_duration.sql
@@ -1,7 +1,5 @@
 SELECT SUM(duration) AS total_duration
 FROM cohorts
-JOIN students
-ON cohort_id = cohorts.id
-JOIN assignment_submissions
-ON students.id = assignment_submissions.student_id
+JOIN students ON cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
 WHERE cohorts.name = 'FEB12';
