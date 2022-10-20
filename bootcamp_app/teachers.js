cde1eee21e5d00f39cc36ce3aafb25149ea0192e
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-20 08:55:54 -0700

    Parametrize node postgres queries for SQL injection prevention

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 
-
 const pool = new Pool({
   user: 'labber',
   password: 'labber',
@@ -8,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const inputs = [`%${process.argv[2]}%`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, inputs)
 
 .then(res => {
   res.rows.forEach(record => {
